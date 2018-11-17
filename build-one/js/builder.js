012ad7477d00b38698fd9fffa2744411dc1dc93c
Alyssa Saucedo <alyssa.saucedo@hulu.com>
2018-11-17 15:23:47 -0800

    added a second build
    removed some stuff and added second build

---
+++ build-one/js/builder.js
@@ -0,0 +1,532 @@
+;( function( window ) {
+	
+	'use strict';
+
+	var support = { animations : Modernizr.cssanimations },
+		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
+		// animation end event name
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
+
+	/**
+	 * extend obj function
+	 */
+	function extend( a, b ) {
+		for( var key in b ) { 
+			if( b.hasOwnProperty( key ) ) {
+				a[key] = b[key];
+			}
+		}
+		return a;
+	}
+
+	/**
+	 * createElement function
+	 * creates an element with tag = tag, className = opt.cName, innerHTML = opt.inner and appends it to opt.appendTo
+	 */
+	function createElement( tag, opt ) {
+		var el = document.createElement( tag )
+		if( opt ) {
+			if( opt.cName ) {
+				el.className = opt.cName;
+			}
+			if( opt.inner ) {
+				el.innerHTML = opt.inner;
+			}
+			if( opt.appendTo ) {
+				opt.appendTo.appendChild( el );
+			}
+		}	
+		return el;
+	}
+
+	/**
+	 * huluBuilder function
+	 */
+	function huluBuilder( el, options ) {
+		this.el = el;
+		this.options = extend( {}, this.options );
+  		extend( this.options, options );
+  		this._init();
+	}
+
+	/**
+	 * huluBuilder options
+	 */
+	huluBuilder.prototype.options = {
+		// show progress bar
+		ctrlProgress : true,
+		// show navigation dots
+		ctrlNavDots : true,
+		// show [current field]/[total fields] status
+		ctrlNavPosition : true,
+		// reached the review and submit step
+		onReview : function() { return false; }
+	};
+
+	/**
+	 * init function
+	 * initialize and cache some vars
+	 */
+	huluBuilder.prototype._init = function() {
+		// the form element
+		this.formEl = this.el.querySelector( 'form' );
+
+		// list of fields
+		this.fieldsList = this.formEl.querySelector( 'ol.hb-fields' );
+
+		// current field position
+		this.current = 0;
+
+		// this.old = -1;
+
+		// all fields
+		this.fields = [].slice.call( this.fieldsList.children );
+		
+		// total fields
+		this.fieldsCount = this.fields.length;
+		
+		// show first field
+		classie.add( this.fields[ this.current ], 'hb-current' );
+
+		// classie.add( this.fields[ this.current  ], 'hb-old' );
+
+		// create/add controls
+		this._addControls();
+
+		// create/add messages
+		this._addErrorMsg();
+		
+		// init events
+		this._initEvents();
+	};
+
+	/**
+	 * addControls function
+	 * create and insert the structure for the controls
+	 */
+	huluBuilder.prototype._addControls = function() {
+		// main controls wrapper
+		this.ctrls = createElement( 'div', { cName : 'hb-controls', appendTo : this.el } );
+
+		// continue button (jump to next field)
+		this.ctrlContinue = createElement( 'button', { cName : 'hb-continue', inner : 'Continue', appendTo : this.ctrls } );
+		this._showCtrl( this.ctrlContinue );
+
+		// navigation dots
+		if( this.options.ctrlNavDots ) {
+			this.ctrlNav = createElement( 'nav', { cName : 'hb-nav-dots', appendTo : this.ctrls } );
+			var dots = '';
+			for( var i = 0; i < this.fieldsCount; ++i ) {
+				dots += i === this.current ? '<button class="hb-dot-current"></button>' : '<button disabled></button>';
+			}
+			this.ctrlNav.innerHTML = dots;
+			this._showCtrl( this.ctrlNav );
+			this.ctrlNavDots = [].slice.call( this.ctrlNav.children );
+		}
+
+		// field number status
+		if( this.options.ctrlNavPosition ) {
+			this.ctrlFldStatus = createElement( 'span', { cName : 'hb-numbers', appendTo : this.ctrls } );
+
+			// current field placeholder
+			this.ctrlFldStatusCurr = createElement( 'span', { cName : 'hb-number-current', inner : Number( this.current + 1 ) } );
+			this.ctrlFldStatus.appendChild( this.ctrlFldStatusCurr );
+
+			// total fields placeholder
+			this.ctrlFldStatusTotal = createElement( 'span', { cName : 'hb-number-total', inner : this.fieldsCount } );
+			this.ctrlFldStatus.appendChild( this.ctrlFldStatusTotal );
+			this._showCtrl( this.ctrlFldStatus );
+		}
+
+		// progress bar
+		if( this.options.ctrlProgress ) {
+			this.ctrlProgress = createElement( 'div', { cName : 'hb-progress', appendTo : this.ctrls } );
+			this._showCtrl( this.ctrlProgress );
+		}
+	}
+
+	/**
+	 * addErrorMsg function
+	 * create and insert the structure for the error message
+	 */
+	huluBuilder.prototype._addErrorMsg = function() {
+		// error message
+		this.msgError = createElement( 'span', { cName : 'hb-message-error', appendTo : this.el } );
+	}
+
+	/**
+	 * init events
+	 */
+	huluBuilder.prototype._initEvents = function() {
+		var self = this;
+
+		// show next field
+		this.ctrlContinue.addEventListener( 'click', function() {
+			self._nextField(); 
+		} );
+
+		// navigation dots
+		if( this.options.ctrlNavDots ) {
+			this.ctrlNavDots.forEach( function( dot, pos ) {
+				dot.addEventListener( 'click', function() {
+					self._showField( pos );
+				} );
+			} );
+		}
+
+		// jump to next field without clicking the continue button (for fields/list items with the attribute "data-input-trigger")
+		this.fields.forEach( function( fld ) {
+			if( fld.hasAttribute( 'data-input-trigger' ) ) {
+				var input = fld.querySelector( 'input[type="radio"]' ) || fld.querySelector( '.cs-select' ) || fld.querySelector( 'select' ); // assuming only radio and select elements (TODO: exclude multiple selects)
+				if( !input ) return;
+
+				switch( input.tagName.toLowerCase() ) {
+					case 'select' : 
+						input.addEventListener( 'change', function() { self._nextField(); } );
+						break;
+
+					case 'input' : 
+						[].slice.call( fld.querySelectorAll( 'input[type="radio"]' ) ).forEach( function( inp ) {
+							inp.addEventListener( 'change', function(ev) { self._nextField(); } );
+						} ); 
+						break;
+
+					
+					// for our custom select we would do something like:
+					case 'div' : 
+						[].slice.call( fld.querySelectorAll( 'ul > li' ) ).forEach( function( inp ) {
+							inp.addEventListener( 'click', function(ev) { self._nextField(); } );
+						} ); 
+						break;
+					
+				}
+			}
+		} );
+
+		// keyboard navigation events - jump to next field when pressing enter
+		document.addEventListener( 'keydown', function( ev ) {
+			if( !self.isLastStep && ev.target.tagName.toLowerCase() !== 'textarea' ) {
+				var keyCode = ev.keyCode || ev.which;
+				if( keyCode === 13 ) {
+					ev.preventDefault();
+					self._nextField();
+				}
+			}
+		} );
+	};
+
+	/**
+	 * nextField function
+	 * jumps to the next field
+	 */
+	huluBuilder.prototype._nextField = function( backto ) {
+		if( this.isLastStep || !this._validade() || this.isAnimating ) {
+			return false;
+		}
+		this.isAnimating = true;
+
+		// check if on last step
+		this.isLastStep = this.current === this.fieldsCount - 1 && backto === undefined ? true : false;
+
+		this._finalScore(true);
+		
+		// clear any previous error messages
+		this._clearError();
+
+		
+
+
+		// current field
+		var currentFld = this.fields[ this.current ];
+
+		// save the navigation direction
+		this.navdir = backto !== undefined ? backto < this.current ? 'prev' : 'next' : 'next';
+
+		// update current field
+		this.current = backto !== undefined ? backto : this.current + 1;
+
+		if( backto === undefined ) {
+			// update progress bar (unless we navigate backwards)
+			this._progress();
+
+			// save farthest position so far
+			this.farthest = this.current;
+		}
+
+		// add class "hb-display-next" or "hb-display-prev" to the list of fields
+		classie.add( this.fieldsList, 'hb-display-' + this.navdir );
+
+		// remove class "hb-current" from current field and add it to the next one
+		// also add class "hb-show" to the next field and the class "hb-hide" to the current one
+		classie.remove( currentFld, 'hb-current' );
+		classie.add( currentFld, 'hb-hide' );
+		// classie.add( currentFld, 'hb-old' );
+		
+		if( !this.isLastStep ) {
+			// update nav
+			this._updateNav();
+
+			// change the current field number/status
+			this._updateFieldNumber();
+
+			var nextField = this.fields[ this.current ];
+			classie.add( nextField, 'hb-current' );
+			classie.add( nextField, 'hb-show' );
+		}
+
+		// after animation ends remove added classes from fields
+		var self = this,
+			onEndAnimationFn = function( ev ) {
+				if( support.animations ) {
+					this.removeEventListener( animEndEventName, onEndAnimationFn );
+				}
+				
+				classie.remove( self.fieldsList, 'hb-display-' + self.navdir );
+				classie.remove( currentFld, 'hb-hide' );
+
+				if( self.isLastStep ) {
+					// show the complete form and hide the controls
+					self._hideCtrl( self.ctrlNav );
+					self._hideCtrl( self.ctrlProgress );
+					// self._hideCtrl( self.ctrlContinue );
+					self._hideCtrl( self.ctrlFldStatus );
+					// replace class hb-bull-full with hb-form-overview
+					classie.remove( self.formEl, 'hb-bull-full' );
+					classie.add( self.formEl, 'hb-form-overview' );
+					classie.add( self.formEl, 'hb-show' );
+					// callback
+					self.options.onReview();
+				}
+				else {
+					classie.remove( nextField, 'hb-show' );
+					
+					if( self.options.ctrlNavPosition ) {
+						self.ctrlFldStatusCurr.innerHTML = self.ctrlFldStatusNew.innerHTML;
+						self.ctrlFldStatus.removeChild( self.ctrlFldStatusNew );
+						classie.remove( self.ctrlFldStatus, 'hb-show-' + self.navdir );
+					}
+				}
+				self.isAnimating = false;
+			};
+
+		if( support.animations ) {
+			if( this.navdir === 'next' ) {
+				if( this.isLastStep ) {
+					currentFld.querySelector( '.hb-anim-upper' ).addEventListener( animEndEventName, onEndAnimationFn );
+				}
+				else {
+					nextField.querySelector( '.hb-anim-lower' ).addEventListener( animEndEventName, onEndAnimationFn );
+				}
+			}
+			else {
+				nextField.querySelector( '.hb-anim-upper' ).addEventListener( animEndEventName, onEndAnimationFn );
+			}
+		}
+		else {
+			onEndAnimationFn();
+		}
+	}
+
+	/**
+	 * showField function
+	 * jumps to the field at position pos
+	 */
+	huluBuilder.prototype._showField = function( pos ) {
+		if( pos === this.current || pos < 0 || pos > this.fieldsCount - 1 ) {
+			return false;
+		}
+		this._nextField( pos );
+	}
+
+	/**
+	 * updateFieldNumber function
+	 * changes the current field number
+	 */
+	huluBuilder.prototype._updateFieldNumber = function() {
+		if( this.options.ctrlNavPosition ) {
+			// first, create next field number placeholder
+			this.ctrlFldStatusNew = document.createElement( 'span' );
+			this.ctrlFldStatusNew.className = 'hb-number-new';
+			this.ctrlFldStatusNew.innerHTML = Number( this.current + 1 );
+			
+			// insert it in the DOM
+			this.ctrlFldStatus.appendChild( this.ctrlFldStatusNew );
+			
+			// add class "hb-show-next" or "hb-show-prev" depending on the navigation direction
+			var self = this;
+			setTimeout( function() {
+				classie.add( self.ctrlFldStatus, self.navdir === 'next' ? 'hb-show-next' : 'hb-show-prev' );
+			}, 25 );
+		}
+	}
+
+	/**
+	 * progress function
+	 * updates the progress bar by setting its width
+	 */
+	huluBuilder.prototype._progress = function() {
+		if( this.options.ctrlProgress ) {
+			this.ctrlProgress.style.width = this.current * ( 100 / this.fieldsCount ) + '%';
+		}
+	}
+
+	/**
+	 * updateNav function
+	 * updates the navigation dots
+	 */
+	huluBuilder.prototype._updateNav = function() {
+		if( this.options.ctrlNavDots ) {
+			classie.remove( this.ctrlNav.querySelector( 'button.hb-dot-current' ), 'hb-dot-current' );
+			classie.add( this.ctrlNavDots[ this.current ], 'hb-dot-current' );
+			this.ctrlNavDots[ this.current ].disabled = false;
+		}
+	}
+
+	/**
+	 * showCtrl function
+	 * shows a control
+	 */
+	huluBuilder.prototype._showCtrl = function( ctrl ) {
+		classie.add( ctrl, 'hb-show' );
+	}
+
+	/**
+	 * hideCtrl function
+	 * hides a control
+	 */
+	huluBuilder.prototype._hideCtrl = function( ctrl ) {
+		classie.remove( ctrl, 'hb-show' );
+	}
+
+	huluBuilder.prototype._finalScore = function(round) {
+		  
+		  var correct = 0;
+		  var selectValue;
+		  
+		  var questions =  document.querySelectorAll('[id^="question"]')
+
+		  // console.log(questions);
+
+		 var numOfQuestions = questions.length;
+
+		 // console.log(numOfQuestions);
+		  
+		for(var i = 0; i < questions.length; i++ ){
+		  
+		 selectValue =  questions[i].options[questions[i].selectedIndex].value;
+		  
+		  if(selectValue === "right"){
+		    correct++;
+		  }
+		}
+
+		
+
+		if(round === false){
+			document.getElementById("scoreDisplay").innerHTML = (100/numOfQuestions) * correct;
+		}
+		else{  
+		 	document.getElementById("scoreDisplay").innerHTML = Math.round((100/numOfQuestions) * correct);
+		}
+
+		var scorescore = document.getElementById("scoreDisplay").innerHTML = Math.round((100/numOfQuestions) * correct);
+		// var vip = document.getElementsByClassName("vip");
+		// var basic = document.getElementsByClassName("basic");
+
+		console.log(scorescore);
+		if(scorescore <= 40) {
+			document.getElementById("basic").style.display = "flex";
+			document.getElementById("vip").style.display = "none";
+			// basic.style.display = "block";
+			// vip.style.display = "none";
+		}
+
+		if (scorescore >= 41) {
+			console.log('you prime');
+			document.getElementById("vip").style.display = "flex";
+			document.getElementById("basic").style.display = "none";
+			// document.getElementsByClassName("basic").style.display = "none";
+			// basic.style.display = "none";
+			// vip.style.display = "block";
+		}
+
+		
+		// } else {
+
+
+		// console.log(selectValue);
+	}
+
+	// TODO: this is a very basic validation function. Only checks for required fields..
+	huluBuilder.prototype._validade = function() {
+		var fld = this.fields[ this.current ],
+			input = fld.querySelector( 'input[required]' ) || fld.querySelector( 'textarea[required]' ) || fld.querySelector( 'select[required]' ),
+			error;
+
+		if( !input ) return true;
+
+		switch( input.tagName.toLowerCase() ) {
+			case 'input' : 
+				if( input.type === 'radio' || input.type === 'checkbox' ) {
+					var checked = 0;
+					[].slice.call( fld.querySelectorAll( 'input[type="' + input.type + '"]' ) ).forEach( function( inp ) {
+						if( inp.checked ) {
+							++checked;
+						}
+					} );
+					if( !checked ) {
+						error = 'NOVAL';
+					}
+				}
+				else if( input.value === '' ) {
+					error = 'NOVAL';
+				}
+				break;
+
+			case 'select' : 
+				// assuming here '' or '-1' only
+				if( input.value === '' || input.value === '-1' ) {
+					error = 'NOVAL';
+				}
+				break;
+
+			case 'textarea' :
+				if( input.value === '' ) {
+					error = 'NOVAL';
+				}
+				break;
+		}
+
+		if( error != undefined ) {
+			this._showError( error );
+			return false;
+		}
+
+		return true;
+	}
+
+	// TODO
+	huluBuilder.prototype._showError = function( err ) {
+		var message = '';
+		switch( err ) {
+			case 'NOVAL' : 
+				message = 'Please fill the field before continuing';
+				break;
+			case 'INVALIDEMAIL' : 
+				message = 'Please fill a valid email address';
+				break;
+			// ...
+		};
+		this.msgError.innerHTML = message;
+		this._showCtrl( this.msgError );
+	}
+
+	// clears/hides the current error message
+	huluBuilder.prototype._clearError = function() {
+		this._hideCtrl( this.msgError );
+	}
+
+	// add to global namespace
+	window.huluBuilder = huluBuilder;
+
+})( window );
